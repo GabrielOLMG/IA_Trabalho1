cd7f5dac7383b7732f45ce452f7c68c905d407c6
GabrielOLMG <gabrielolmg@gmail.com>
2021-03-30 00:27:22 +0100

    Grafico com cor

--- Codigo/Questao5.java
+++ Codigo/Questao5.java
@@ -7,7 +7,7 @@ public class Questao5 extends Questao4{
     public void simulated_annealing(char f, Ponto[] candidato ){ 
         Ponto[] atual = new Ponto[candidato.length];
         System.arraycopy(candidato, 0, atual, 0, candidato.length);
-        double alfa = 0.95; // o quanto a temperatura vai ser diminuida por turno
+        double alfa = 0.99; // o quanto a temperatura vai ser diminuida por turno
         double T = 100; // temperatura inicial
         int i = 0;
         while(true){
@@ -18,7 +18,7 @@ public class Questao5 extends Questao4{
             }
             Ponto[] Vizinho = proximo(atual,f);
             System.out.println("Atual: " + Arrays.toString(atual) + " Vizinho: " + Arrays.toString(Vizinho));
-            System.out.println(Qconflitos(atual) + " " + Qconflitos(Vizinho) );
+            System.out.println(Qconflitos(atual) + " " + Qconflitos(Vizinho) + "-----> " + T );
             if(Qconflitos(atual)==0){ //faz com q ele pare quando chega a um max d interações
                 arquivo.escreveCoordenada(Vizinho);
                 System.out.println("Fim " + i);
@@ -27,7 +27,7 @@ public class Questao5 extends Questao4{
             int varEnrg = Qconflitos(Vizinho) - Qconflitos(atual);
             if(varEnrg < 0)  System.arraycopy(Vizinho, 0, atual, 0, candidato.length);
             else{
-                double p = Math.random();
+                double p = Math.random(); //OBS:SEMPRE Q VAR=0 ELE ESTA ACEITANDO O VALOR VIZINHO, JA Q E^(-DELTA/T) = 1.
                 if(p < Math.exp(-varEnrg/T)){ //vizinho teve mais conflitos, então vamos verificar se devemos considerar pegar ele ou n, usando probabilidade e^(-VariancaDoConflito/T)
                     System.arraycopy(Vizinho, 0, atual, 0, candidato.length);
                     System.out.println("So desta vez ein " + Math.exp(-varEnrg/T));
